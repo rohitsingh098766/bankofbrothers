7c38529394c9bab650f54544ceb72de6fc839819
rohitsingh098766 <73010829+rohitsingh098766@users.noreply.github.com>
2022-07-28 11:15:30 +0530

    r

---
+++ sql.php
@@ -0,0 +1,17 @@
+<?php
+     session_start();
+     include './connection.php';
+
+
+     <?php
+                    
+     $query = "select * from users ";
+$query = mysqli_query($connection,$query);
+while($row = mysqli_fetch_assoc($query)){
+
+echo  $row['user_name']." is users ";
+
+}
+
+
+?>
