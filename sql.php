24a53508cb5607d9e6b8d2b411e0feca1fc5bb0e
rohitsingh098766 <73010829+rohitsingh098766@users.noreply.github.com>
2022-07-28 11:24:35 +0530

    Update sql.php

--- sql.php
+++ sql.php
@@ -7,13 +7,14 @@ echo "atleast print now";
 
    
                     
-//      $query = "select * from user";
-// $query = mysqli_query($connection,$query);
-// while($row = mysqli_fetch_assoc($query)){
+     $query = "select * from user";
+$query = mysqli_query($connection,$query);
+while($row = mysqli_fetch_assoc($query)){
 
 // echo  $row['user_name']." is users ";
 
-// }
+echo "bro ";
+}
 
 
 ?>
